2438208365f327ce2946223543ed279279598698
smelyiAis <124904635+smelyiAis@users.noreply.github.com>
2023-05-12 01:02:59 +0600

    Add files via upload

---
+++ BST.java
@@ -0,0 +1,108 @@
+import java.util.Queue;
+import java.util.LinkedList;
+public class BST<K extends Comparable<K>, V> {
+    private Node root;
+
+    private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    public void put(K key, V val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node x, K key, V val) {
+        if (x == null) {
+            return new Node(key, val);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, val);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, val);
+        } else {
+            x.val = val;
+        }
+        return x;
+    }
+
+    public V get(K key) {
+        Node x = root;
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0) {
+                x = x.left;
+            } else if (cmp > 0) {
+                x = x.right;
+            } else {
+                return x.val;
+            }
+        }
+        return null;
+    }
+
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, K key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = delete(x.left, key);
+        } else if (cmp > 0) {
+            x.right = delete(x.right, key);
+        } else {
+            if (x.left == null) {
+                return x.right;
+            }
+            if (x.right == null) {
+                return x.left;
+            }
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        return x;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) {
+            return x;
+        }
+        return min(x.left);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) {
+            return x.right;
+        }
+        x.left = deleteMin(x.left);
+        return x;
+    }
+
+    public Iterable<K> iterator() {
+        Queue<K> keys = new LinkedList<>();
+        inorder(root, keys);
+        return keys;
+    }
+
+    private void inorder(Node x, Queue<K> keys) {
+        if (x == null) {
+            return;
+        }
+        inorder(x.left, keys);
+        keys.add(x.key);
+        inorder(x.right, keys);
+    }
+}
